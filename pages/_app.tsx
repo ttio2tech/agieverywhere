1f1ae77970359c07d6ddbc6b84220e4f4aa3a16f
tio2 <pema@syr.edu>
2023-08-25 08:53:24 -0400

    add vercel analytics code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
+  )
 }
